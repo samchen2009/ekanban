88aa407a10a1ed8b365f0c96d956e9ecd7afef65
shchen <samchen2009@gmail.com>
2013-08-24 01:09:14 +0800

    only showed valid kanban board in setting, now show all to make re-enabled possible

--- lib/ekanban/redmine_patches.rb
+++ lib/ekanban/redmine_patches.rb
@@ -9,7 +9,8 @@ module EKanban
           unloadable
 
           def kanbans
-            @kanbans = Kanban.where("project_id=? and is_valid = ?",@project.id, true)
+            #@kanbans = Kanban.where("project_id=? and is_valid = ?",@project.id, true)
+            @kanbans = Kanban.where("project_id=? ",@project.id)
           end
           #user to extend.
           #alias_method_chain :show, :plugin
